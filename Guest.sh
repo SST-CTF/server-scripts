e98f8d37e90db00393c5e507547f5fef6942278f
root <root@sstctf.us.to>
2016-10-07 01:45:38 -0700

    Added Guest Script

--- Guest.sh
+++ Guest.sh
@@ -1,19 +1,44 @@
-# Clear out the home
-cd /home/guest
+#!/bin/bash 
+#
+# This script clears the guest home directory, sets a random new password
+# and then makes sure the admins have quick and easy access to this pass.
+#
+# *MUST BE ROOT FOR SCRIPT TO WORK*
+#
+# 10/7/2016
+# Otakar Andrysek
+# SST CTF
+#
+
+# Variables for easy adaptability
+ACCOUNT=guest
+DATE=$(date --date="next day" +%A" "%B" "%d" "%Y)
+TXT_FILE_NAME="GuestPass"
+EMAIL_SUBJECT="Guest Account Password"
+EMAIL_TO="sstctf@gmail.com"
+
+# Clean up
+killall -u $ACCOUNT
+cd /home/$ACCOUNT
 rm -r *
 
 # Create a new password
-initialize_password_change="passwd guest"
-eval $initialize_password_change
-#Usually don't use this command, only use set_guest_password
+NEW_PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+echo "The new guest account for $DATE is: "
+echo $NEW_PASS
 
-generate_new_password='eval [Insert Random Password Generating Program]'
-eval $generate_new_password
+# Set the new password
+echo "$ACCOUNT:$NEW_PASS" | chpasswd
 
-set_guest_password="$initialize_password_change [Insert Input Command Data From ->] $generate_new_password"
-
-# Print the password to the /root folder
-echo $set_guest_password [To /Root Folder]
+# Print the password into a file to the /root folder
+cat >/root/$TXT_FILE_NAME <<EOL
+Account: $ACCOUNT
+Date: $DATE
+Password: $NEW_PASS
+EOL
+cat /root/$TXT_FILE_NAME
 
 # Email the password, too.
-eval $sstmp [Insert Email Command, then Input To:, From:, Subject:, then Quit]
+cat /root/$TXT_FILE_NAME | mail -s "$EMAIL_SUBJECT" $EMAIL_TO
+
+#END
